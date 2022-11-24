901731173f555a64cf086de568b2734b5460eea7
Pratiyush-202107 <Pratiyushpk@gmail.com7>
2022-11-24 23:30:34 +0530

    find the arrangement of N queens

---
+++ backtrackingNQueens.py
@@ -0,0 +1,47 @@
+# Program to find the arrangement of N queens such
+# that no two queens lie in same row, column or diagonal.
+
+def is_attack(i, j):
+    # Checking if there is a queen in row or column
+    for k in range(0,N):
+        if board[i][k] == 1 or board[k][j] == 1:
+            return True
+
+    # Checking diagonals
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k + l == i + j) or (k - l == i - j):
+                if board[k][l] == 1:
+                    return True
+    return False
+
+
+def N_queen(n):
+    # Base case
+    if n == 0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            '''checking if we can place a queen here or not
+            queen will not be placed if the place is being attacked
+            or already occupied'''
+            if (not(is_attack(i,j))) and (board[i][j] != 1):
+                board[i][j] = 1
+                #recursion
+                #wether we can put the next queen with this arrangment or not
+                if N_queen(n - 1) == True:
+                    return True
+                board[i][j] = 0
+
+    return False
+
+#  Driver code
+print ("Enter the number of queens:")
+N = int(input())
+
+# Chessboard (NxN matrix with all elements 0)
+board = [[0]*N for _ in range(N)]
+
+N_queen(N)
+for i in board:
+    print (i)
